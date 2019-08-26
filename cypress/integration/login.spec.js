e4c532693abfac807d7a2160a01668359620eeec
Andreas Berrou <BEA@neusta.de>
2019-08-26 10:03:02 +0200

    added spt-test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,8 @@
 describe('spt-membership-tests', () => {
-  it('logs in as a admin into membership', () => {
+  it('logs in as a admin into membership and logs out again', () => {
     cy.visit('http://membership-fe-nspmvdev.nspmv.intern.neusta.de/login')
     cy.get('#username').type('membership_admin')
     cy.get('#password').type('1234')
     cy.contains('einloggen').click()
-  })
+    cy.contains('Logout').click()
 })
